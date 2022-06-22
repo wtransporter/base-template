33a3ea09884fc96a43a2c00508541ee282b5f298
vlada <vladapantic@hotmail.com>
2022-06-22 11:08:55 +0200

    Initial commit - main page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1a4c97ec'
+      }
+    },
+  },
+  plugins: [],
+}
